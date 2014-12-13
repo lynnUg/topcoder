8fdfcd5b37f876c8496eac723247be6878c9e247
lynnug <lynnasiimwe@gmail.com>
2014-12-13 17:36:35 +0300

    adding cost of dancing problem

---
+++ CostOfDancing.py
@@ -0,0 +1,37 @@
+"""
+
+
+
+ Problem Statement for CostOfDancing
+
+
+Problem Statement
+    	
+Gustavo studies at the Byteversity (one of the best universities in Byteland). He's also very keen on dancing, so he decided to enroll at a dance school.
+
+
+
+The school offers many different courses, each teaching one dance. Different courses may have different costs. You are given a int[] danceCost. The elements of danceCost are the costs of all courses offered at the dance school.
+
+
+
+Gustavo would like to learn exactly K of those dances. He is a poor student, so his only priority is to pay as little as possible for the courses.
+
+
+
+You are given the int K and the int[] danceCost. Compute and return the smallest possible total cost of learning K dances.
+
+ 
+Definition
+    	
+Class:	CostOfDancing
+Method:	minimum
+Parameters:	int, int[]
+Returns:	int
+Method signature:	int minimum(int K, int[] danceCost)
+(be sure your method is public)
+
+"""
+class CostOfDancing(object):
+	def minimum(self,K,danceCost):
+		sum(sorted(danceCost)[:K])
